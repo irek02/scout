6c4a6995c3380a51480b972649cce3a5653bcca0
Irek Mirgaleev <irek.mirgaleev@acquia.com>
2015-06-06 16:40:44 -0400

    Trying to make the OO approach work

--- main.py
+++ main.py
@@ -1,75 +1,41 @@
-import io
 import os.path
 import RPi.GPIO as GPIO
 import time
 import picamera
 import vehicle
-from PIL import Image
+import imageprocessor
+
+
+GPIO.setmode(GPIO.BCM)
+
+pin = 18
+pin_led = 15
+
+GPIO.setup(pin, GPIO.OUT)
+GPIO.output(pin,GPIO.LOW)
+
+GPIO.setup(pin_led, GPIO.OUT)
+GPIO.output(pin_led,GPIO.LOW)
+
+# Set the resolution and target boundaries.
+res_w = 50
+res_h = 40
 
 with picamera.PiCamera() as camera:
 	camera.resolution = (res_w, res_h)
 	camera.framerate = 10
 	camera.start_preview()
-	time.sleep(2)
+	#time.sleep(2)
 
-	img_processor = ImageProcessor(camera)
+	img_processor = imageprocessor.ImageProcessor(camera)
+	print("hey2")
 	vehicle = vehicle.Vehicle(img_processor)
+	print("hey3")
 	vehicle.seek_and_destoy()
 
+# Shutdown the laser.
+GPIO.output(pin, GPIO.LOW)
+GPIO.output(pin_led, GPIO.LOW)
 
-class ImageProcessor:
-	def __init__(self, camera):
-		self.stream = io.BytesIO()
-		self.target_loc = None
-        camera.capture_sequence(stream_generator(), use_video_port=True)
-
-	def stream_generator(self):
-	    while True:
-	        stream.truncate()
-	        stream.seek(0)
-	        yield stream
-	        target_loc = calc_target_loc()
-
-	def get_target_loc(self):
-		return target_loc
-
-	def getPixels(self):
-		return Image.open(self.stream).load()
-
-	def calc_target_loc(self):
-	 
-	    pixels = getPixels()
-	    
-	    x_cluster = []
-	    y_cluster = []
-	    for x in range(1, res_w):
-	        for y in range(1, res_h):
-	            if (sum(pixels[x,y]) < 55):
-	                x_cluster.append(x)
-	                y_cluster.append(y)
-
-	    if (len(x_cluster) == 0 or len(y_cluster) == 0):
-	        return
-	    
-	    x_center = round(sum(x_cluster)/len(x_cluster))
-	    y_center = round(sum(y_cluster)/len(y_cluster))
-
-	    if (y_center not in y_range):
-	        GPIO.output(pin_led, GPIO.LOW)
-	        on_target = 0
-	        if (y_center < y_range[0]):
-	            return 'up'
-	        else:
-	            return 'down'
-
-	    if (x_center not in x_range):
-	        GPIO.output(pin_led, GPIO.LOW)
-	        on_target = 0
-	        if (x_center < x_range[0]):
-	            return 'left'
-	        else:
-	            return 'right'
-	    
-	    return 'center'
-
+GPIO.cleanup()
 
--- vehicle.py
+++ vehicle.py
@@ -1,42 +1,60 @@
+import time
+import RPi.GPIO as GPIO
+
 class Vehicle:
-	def __init__(self, img_processor):
-    	self.img_processor = img_processor
-    	self.on_target = 0
-    	self.target_engaged = False
-
+    def __init__(self, img_processor):
+        self.img_processor = img_processor
+        self.on_target = 0
+        self.target_engaged = False
+        self.target_lock_time = 2
+        self.led = Pin(15)
+        self.laser = Pin(18)
+        
     def seek_and_destroy(self):
-    	while not target_engaged:
-	    	target_loc = img_processor.get_target_loc()
+        print("seek")
+        while not self.target_engaged:
+            target_loc = img_processor.get_target_loc()
 
-	    	if not target_loc:
-	    		print("Target not found")
-	    		continue
+            if not target_loc:
+                print("Target not found")
+                continue
 
-	    	if target_loc == 'center':
-	    		if (on_target == 0):
-			        on_target = time.time()
-			        GPIO.output(pin_led, GPIO.HIGH)
-			        continue
-			    elif (time.time() - on_target > target_lock_time):
-			        engage_target()
-			        on_target = 0
-			        print('Target engaged.')
-			        return
+            if target_loc == 'center':
+                self.led.on()
+                if (self.on_target == 0):
+                    self.on_target = time.time()
+                elif (time.time() - self.on_target > self.target_lock_time):
+                    self.engage_target()
+                    on_target = 0
+                    print('Target engaged.')
+                    return
+                continue
 
-	    	if target_loc in ['up', 'down', 'left', 'right']:
-	    		move(target_loc)
-	    		continue
+            self.led.off()
 
-	def engage_target(self):
-		GPIO.output(pin,GPIO.HIGH)
-		time.sleep(0.5)
-		GPIO.output(pin,GPIO.LOW)
-		GPIO.output(pin_led, GPIO.LOW)
-		target_engaged = True
+            if target_loc in ['up', 'down', 'left', 'right']:
+                self.move(target_loc)
+                continue
 
+    def engage_target(self):
+        self.laser.on()
+        time.sleep(0.5)
+        self.laser.off()
+        self.led.off()
+        self.target_engaged = True
 
-	
+    def move(self, direction):
+        # Implement flight control here
+        direction
 
-	def move(self, direction):
-		# Implement flight control here
-		direction
+class Pin:
+    def __init__(self, pin):
+        self.pin = pin
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(self.pin, GPIO.LOW)
+
+    def on(self):
+        GPIO.output(self.pin, GPIO.HIGH)
+
+    def off(self):
+        GPIO.output(self.pin, GPIO.LOW)
