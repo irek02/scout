cc4724cb89f064b3c274355879bf9b5e5d7ea5b6
Irek Mirgaleev <irek.mirgaleev@acquia.com>
2015-06-13 18:39:47 -0400

    Add some pretty white space

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import time
 
 resolution = (50, 40)
 
-camera = components.Camera(resolution)
+camera     = components.Camera(resolution)
 target_loc = components.TargetLocator(resolution)
-led = components.Pin(15)
-laser = components.Pin(18)
+led        = components.Pin(15)
+laser      = components.Pin(18)
 
 time.sleep(1)
 
